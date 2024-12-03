59620f0ff36eac38acd694af7cb7ab1091a4915b
Diego Aguilar <diego.alfonso.aguilar.cardona@gmail.com>
2024-12-02 21:08:37 -0500

    create bin folder if it does not exists

--- face_matcher/build_face_matcher.sh
+++ face_matcher/build_face_matcher.sh
@@ -1,4 +1,7 @@
    #!/bin/bash
-
+   
+   # Create the bin directory if it does not exist
+   mkdir -p bin
+   
    # Compile face_matcher.cpp with g++
    g++ face_matcher.cpp -std=c++11 -o ./bin/face_matcher `pkg-config --cflags --libs opencv4`
