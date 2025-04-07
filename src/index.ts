0299e318a8d5d9373f93fc9184e6fa5a2fa505ce
Gabriel Mata <gabrielmatau@gmail.com>
2025-04-07 17:55:25 -0500

    feat: Upgrade logger to index.ts
    Signed-off-by: Gabriel Mata <gabrielmatau@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { createApp } from './app'
+import { log } from './utils/logger'
 
 const app = createApp()
 const port = process.env.PORT || 5123
 
 const server = app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
+  log(`Server is running on http://localhost:${port}`)
 })
 
 server.on('close', () => {
-  console.log('Server shutting down...')
+  log('Server shutting down...')
 })
 
 process.on('exit', () => {
