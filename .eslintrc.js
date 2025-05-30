b8ed91bece6a36cdfd54fa5f4f8293a2367713a5
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-05-30 12:49:48 -0500

    fix: add ignore linter
    Signed-off-by: Andres Vallecilla <andresfelipe083195@hotmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'packages/client/src/index.d.ts'],
+  ignorePatterns: ['.eslintrc.js', 'packages/client/src/index.d.ts', 'charts/*'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
